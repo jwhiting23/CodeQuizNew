4e83fc2eea879d5672c3d682ed9596b12797698f
Jonathan Whiting <jonathan_whiting@ymail.com>
2021-10-31 17:51:22 -0400

    first push

---
+++ questions.js
@@ -0,0 +1,37 @@
+// variable for questions, choices and answers
+
+var questions = [
+    {
+      title: "Which of these are a programming language?",
+      choices: ["JavaScript", "French", "Russian", "Array"],
+      answer: "JavaScript"
+    },
+    {
+      title:
+        "What tag can be used to insert a line break or blank line in an HTML document?",
+      choices: ["<body></body>", "<title></title>", "<br></br>", "<section></section>"],
+      answer: "<br></br>"
+    },
+    {
+      title: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
+      choices: ["Recorders", "Arrays", "Variables", "Strings"],
+      answer: "Array"
+    },
+    {
+      title:
+        "HTML is an acronym of:",
+      choices: ["HyperText Markup Language", "HyperText Machine Language", "HyperToken Markup Language", "HyperToken Machine Language"],
+      answer: "HyperText Markup Language"
+    },
+    {
+      title: "CSS is an acronym of",
+      choices: ["Coded Style Sheets", "Cascading Style Sheets", "Coding Style Sheets ", "Cascaded Style Sheets"],
+      answer: "Cascading Style Sheets"
+    },
+    {
+      title:
+        "In HTML, an unordered list is created using which tag?",
+      choices: ["<UL>", "<NL>", "<OL>", "<AL>"],
+      answer: "<UL>"
+    }
+  ];
